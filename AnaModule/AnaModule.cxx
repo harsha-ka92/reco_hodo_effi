fb7f74e507c278a24dc7eeffa9ee4522a510d106
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-08 13:08:37 -0600

    Update AnaModule.cxx

--- AnaModule/AnaModule.cxx
+++ AnaModule/AnaModule.cxx
@@ -183,7 +183,6 @@ void AnaModule::MakeTree()
   saveTree = new TTree("save", "Efficiency tree Created by AnaModule");
   	saveTree->Branch("eventID", &eventID, "eventID/I");
 	saveTree->Branch("runID", &runID,"runID/I");
-	saveTree->Branch("dor", &dor,"dor/I");
 	saveTree->Branch("trigger", &trigger, "trigger/I");
   	saveTree->Branch("detID", &detID, "detID/I");
  	saveTree->Branch("eleID_exp", &eleID_exp, "eleID_exp/I");
