d7810de78225312bbb9924ce3b7091f44fa6b577
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-09 16:29:39 -0600

    Create test.C

---
+++ AnaModule/test.C
@@ -0,0 +1,25 @@
+void test(){
+  std::map<std::string, std::vector<float>> runlist;
+  
+  std::int runID;
+  runlist ID;
+
+  std::ifstream infile("mysql_output.txt");
+
+  while (infile >> ID)
+  {
+      // See if the key/value pair is already
+      // in the map
+    runlist::iterator it = ID.find(runID);
+
+      // If it is present, increment the count (value)
+    if (it != ID.end())
+      it->second++;  // Same as: (*it).second++
+    else
+    {
+        // Create a new pair with value set to 1
+      std::pair<std::string, int> pr(runID, 1);
+      ID.insert(pr);
+    }
+  }
+}
