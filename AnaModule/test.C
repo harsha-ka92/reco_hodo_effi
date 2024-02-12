ae685c27f7f1b703e09a2c4143067a1d66f6185d
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-12 09:33:24 -0600

    Update test.C

--- AnaModule/test.C
+++ AnaModule/test.C
@@ -29,7 +29,7 @@ void test(){
   for (auto it = run_time.begin(); it != run_time.end(); it++){
   cout<< "runID " << it->first << endl;
   cout<< "begin time " << it->second.first << " s"<< endl;
-  cout<< "rend time " << it->second.second << " s"<< endl;
-  cout<< "duration " << it->second.first - it->second.second << " s"<< endl;
+  cout<< "end time " << it->second.second << " s"<< endl;
+  cout<< "duration " << it->second.second - it->second.first << " s"<< endl;
   }
 }
