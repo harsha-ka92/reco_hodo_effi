ce4221ad2fb1f0e7c3a9fe3fcb648a1596253d8e
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-12 13:21:46 -0600

    Update AnaModule.cxx

--- AnaModule/AnaModule.cxx
+++ AnaModule/AnaModule.cxx
@@ -39,7 +39,7 @@ int AnaModule::Init(PHCompositeNode* topNode)
   return Fun4AllReturnCodes::EVENT_OK;
 }
 
-int AnaModule::InitRun(PHCompositeNode* topNode, std::map)
+int AnaModule::InitRun(PHCompositeNode* topNode)
 {
   int ret = GetNodes(topNode);
   if(ret != Fun4AllReturnCodes::EVENT_OK) return ret;
