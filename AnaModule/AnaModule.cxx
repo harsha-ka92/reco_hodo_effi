e706488433f4876aa694070d7ba2fb2293c91a9f
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-19 15:41:12 -0600

    Update AnaModule.cxx

--- AnaModule/AnaModule.cxx
+++ AnaModule/AnaModule.cxx
@@ -78,7 +78,7 @@ int AnaModule::process_event(PHCompositeNode* topNode)
 	/*for (auto iter = hodo_planes.begin(); iter != hodo_planes.end(); iter++){
 		ID = *iter;*/
 		auto vec = UtilSQHit::FindHitsFast(event, hitVector, "H1T");
-		std::cout << "Number of Hits in H1T "=  << vec->size() << std::endl;
+		std::cout << "Number of Hits in H1T = "  << vec->size() << std::endl;
  			for (auto it = vec->begin(); it != vec->end(); it++) {
 				//int    ele_id = (*it)->get_element_id();
 				double tdc_time   = (*it)->get_tdc_time  ();
