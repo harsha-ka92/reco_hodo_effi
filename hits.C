af3973753cd9cbbc0254aa6865233200115753c9
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-21 11:56:47 -0600

    Update hits.C

--- hits.C
+++ hits.C
@@ -140,8 +140,8 @@ for (int i_ent = 0; i_ent < tr->GetEntries(); i_ent++) {
       tr->GetEntry(i_ent);
 
       //remove the "continue" of the trigger you want in the analysis
-      if(trigger == 1) {continue; trigger_temp = "NIM1";}  //NIM1
-      if(trigger == 2) {trigger_temp = "NIM2";}  //NIM2
+      if(trigger == 1) { trigger_temp = "NIM1";}  //NIM1
+      if(trigger == 2) {continue; trigger_temp = "NIM2";}  //NIM2
       if(trigger == 3) {continue; trigger_temp = "NIM3";}  //NIM3
       if(trigger == 4) {continue; trigger_temp = "NIM4";}  //NIM4
       if(trigger == 5) {continue; trigger_temp = "MATRIX5";}  //MATRIX5
