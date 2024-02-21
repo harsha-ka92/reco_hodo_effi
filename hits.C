d94dfecda036f78da530ba719b000cea20bf1dd5
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-21 13:13:41 -0600

    Update hits.C

--- hits.C
+++ hits.C
@@ -144,8 +144,8 @@ for (int i_ent = 0; i_ent < tr->GetEntries(); i_ent++) {
           if(trigger == 1) {continue; trigger_temp = "NIM1";}  //NIM1
           if(trigger == 2) {continue; trigger_temp = "NIM2";}  //NIM2
           if(trigger == 3) {continue; trigger_temp = "NIM3";}  //NIM3
-          if(trigger == 4) { trigger_temp = "NIM4";}  //NIM4
-          if(trigger == 5) {continue; trigger_temp = "MATRIX5";}  //MATRIX5
+          if(trigger == 4) {continue; trigger_temp = "NIM4";}  //NIM4
+          if(trigger == 5) { trigger_temp = "MATRIX5";}  //MATRIX5
       }
         
       if(run_ID < xlow || run_ID > xhigh) {continue;}
