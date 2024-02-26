3f9975f65d5587e6359e3224e0b3da2eb3a1cd8f
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-26 12:17:58 -0600

    Update and rename test.C to Spill_Info.C

---
+++ Spill_Info.C
@@ -0,0 +1,37 @@
+#include<iostream> 
+#include<map>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+void test(){
+
+  struct run{
+      int runID;
+      int spillID;
+      int bos_vme;
+      int eos_vme;
+  };
+
+
+  map<pair<int,int>, pair<int,int>> spill_time;
+
+  string id;
+  ifstream infile("mysql_output_spill_info.txt");
+
+  while (getline(infile,id))
+  {
+ 
+    istringstream is( id );
+    run r;
+    is >> r.runID >> r.spillID >> r.bos_vme >> r.eos_vme;
+   
+    spill_time[std::pair<int,int>(r.runID, r.spillID)] = std::pair<int,int>(r.bos_vme, r.eos_vme); 
+    
+    }
+
+  for (auto it = spill_time.begin(); it != spill_time.end(); it++) {
+    cout << it->first.first << "\t" << it->first.second << "\t" << it->second.first << "\t" << it->second.second << "\n";
+  }
+}
