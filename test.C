94ad4aa3882bdb3889f2bd9bacbb0fc85650acbe
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-02-26 12:03:52 -0600

    Add files via upload

---
+++ test.C
@@ -0,0 +1,47 @@
+#include<iostream> 
+#include<map>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+void test(){
+  
+  int i=1;
+  
+  struct run{
+      int runID;
+      int beginT;
+      int endT;
+  };
+
+
+  map<int, int> bT;
+  map<int, int> eT;
+
+  string id;
+  ifstream infile("mysql_output.txt");
+
+  while (getline(infile,id))
+  {
+ 
+    istringstream is( id );
+    run r;
+    string s;
+
+    getline( is, s, '\t' );
+    r.runID = stoi( s );
+    getline( is, s, '\t' );
+    r.beginT = stoi( s );
+    getline( is, s, '\t' );
+    r.endT = stoi( s );
+   
+    bT.insert(pair<int, int>(r.runID, r.beginT));
+    eT.insert(pair<int, int>(r.runID, r.endT));
+    
+    cout<<"begin times "<<bT[i]<<endl;
+    cout<<"end times "<<eT[i]<<endl;
+    cout<<i<<endl;
+    i++; 
+    }
+}
