bfb7dfd429a4f23befc4c0080973639465bc58f2
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-03-21 12:21:32 -0500

    Update paddleEffi.C

--- paddleEffi.C
+++ paddleEffi.C
@@ -18,7 +18,7 @@ R__LOAD_LIBRARY(libanamodule)
 using namespace ROOT::VecOps;
 using namespace std;
 
-Void getEffi(int ID, int nPaddles, int cut);
+void getEffi(int ID, int nPaddles, int cut);
 
 void paddleEffi()
 {
@@ -86,9 +86,9 @@ void paddleEffi()
     getEffi(43, 16, 10); getEffi(44, 16, 10); getEffi(45, 16, 10); getEffi(46, 16, 10);
 }
 
-Void getEffi(int ID, int nPaddles, int cut){
-    TH1D *exps = new TH1F("exps","exps",nPaddles+1, 0.5, nPaddles+0.5);
-    TH1D *closest = new TH1F("closest","closest", nPaddles+1, 0.5, nPaddles+0.5);
+void paddleEffi::getEffi(int ID, int nPaddles, int cut){
+    TH1D *exps = new TH1D("exps","exps",nPaddles+1, 0.5, nPaddles+0.5);
+    TH1D *closest = new TH1D("closest","closest", nPaddles+1, 0.5, nPaddles+0.5);
     TH1D *diff = new TH1F("diff","difff", 11, -5.5, 5.5);
     TCanvas* c1 = new TCanvas("c1", "");
     c1->SetGrid();
