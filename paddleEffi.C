4f7a138f5d5c346d03c3f7eabc7faa1a8272ab52
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-03-29 10:56:01 -0500

    Update paddleEffi.C

--- paddleEffi.C
+++ paddleEffi.C
@@ -182,8 +182,7 @@ void getEffi(TTree* evtTree, TTree* tlsTree, int ID, int nPaddles, int cut){
     if(ID>38 && ID < 41){
             for(int i_tls_entry =0;  i_tls_entry < tr_tls->GetEntries(); i_tls_entry++){
                 tr_tls->GetEntry(i_tls_entry);
-                if (chisq/nHits > 3) {continue;}
-                if(stID == 4 || stID == 5){
+                if(stID == 6 && chisq < 8){
                 if (event_ID == tls_event_ID){ 
                         for ( int j =0; j< detIDs->size(); j++){
                                 if(detIDs->at(j) == ID){
@@ -207,8 +206,7 @@ void getEffi(TTree* evtTree, TTree* tlsTree, int ID, int nPaddles, int cut){
     else if (ID >40 && ID < 47){
         for(int i_tls_entry =0;  i_tls_entry < tr_tls->GetEntries(); i_tls_entry++){
                 tr_tls->GetEntry(i_tls_entry);
-                if (chisq/nHits > 3) {continue;}
-                if(stID == 4 || stID ==5){
+                if(stID == 6  && chisq < 8){
                 if (event_ID == tls_event_ID){ 
                         for ( int j =0; j< detIDs->size(); j++){
                                 if(detIDs->at(j) == ID){
