d3e79a5fb0222453a61250a5b665d385bc1c2f7e
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-04-10 14:12:17 -0500

    Update paddleEffi.C

--- paddleEffi.C
+++ paddleEffi.C
@@ -92,8 +92,8 @@ void getEffi(TTree* evtTree, TTree* tlsTree, int ID, int nPaddles, int cut){
 
       //use NIM 4 events to get st3 and st4 efficiencies. And NIM 2 events for st1 and st2
       if (ID < 31) {std::cout<<"Invalid station ID"<<std::endl; break;}
-      if (ID>30 && ID<39 && (trigger & 0x2) == 0 ){continue;} //select NIM2
-      if (ID>38 && ID<47 && (trigger & 0x8) == 0 ){continue;} //select NIM4
+      if ((ID>30 && ID<39) && ((trigger & 0x2) == 0) ){continue;} //select NIM2
+      if ((ID>38 && ID<47) && ((trigger & 0x8) == 0) ){continue;} //select NIM4
       if (ID > 47) {std::cout<<"Invalid station ID"<<std::endl; break;}
 
       std::cout<<"event is selected"<<std::endl;
