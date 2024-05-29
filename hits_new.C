79fa7cfb22e9d82874342fd11e57698369d8af81
Harsha Kaluarachchi <66855050+harsha-ka92@users.noreply.github.com>
2024-05-29 11:07:03 -0500

    Update hits_new.C

--- hits_new.C
+++ hits_new.C
@@ -1,116 +1,136 @@
-R__LOAD_LIBRARY(libinterface_main)
-R__LOAD_LIBRARY(libfun4all)
-R__LOAD_LIBRARY(libg4detectors)
-R__LOAD_LIBRARY(libg4eval)
-R__LOAD_LIBRARY(libktracker)
-R__LOAD_LIBRARY(libanamodule)
-
-#include <ROOT/RDataFrame.hxx>
-#include <ROOT/RVec.hxx>
+#include <iomanip>
 #include <TFile.h>
-#include <TEfficiency.h>
-#include <TGraphAsymmErrors.h>
-#include <TString.h>
-#include <TCanvas.h>
-#include <iostream>
+#include <TTree.h>
+#include <fstream>
 
-#include "hits.h"
+#include <fun4all/Fun4AllReturnCodes.h>
+#include <phool/getClass.h>
+#include <interface_main/SQHitVector_v1.h>
+#include <interface_main/SQTrackVector_v1.h>
+#include <interface_main/SQDimuonVector_v1.h>
+#include <interface_main/SQRun.h>
+#include <interface_main/SQEvent.h>
+#include <UtilAna/UtilSQHit.h>
+#include <vector>
 
-using namespace ROOT::VecOps;
-using namespace std;
-using ROOT::RDataFrame;
+#include "AnaModule.h"
 
-void hits_new()
-{
+AnaModule::AnaModule(const std::string& name): SubsysReco(name), p_geomSvc(GeomSvc::instance())
+{}
 
-    gStyle->SetOptStat(0);
-    gStyle->SetOptFit(1);
+AnaModule::~AnaModule()
+{}
 
-    TFile *f_file = TFile::Open("ana.root","READ");
-    TTree *tr = (TTree*) f_file->Get("save");
-    TTree *tr_tls = (TTree*) f_file->Get("tls");
-
-   // choose the range of run numbers need to be analyzed and show up in the plots
-   int xlow = 5469;
-   int xhigh = 5471; 
-
-   //set the first run_ID in the save tree as the run_num
-   int run_num = 5470;
-
-   //ref to keep track of the entry from the tls tree
-   int tls_entry = 0; 
-
-   tr->SetBranchAddress("run_ID", &run_ID);
-   tr->SetBranchAddress("event_ID", &event_ID);
-   tr->SetBranchAddress("dor", &dor);
-   tr->SetBranchAddress("trigger", &trigger);
-   tr->SetBranchAddress("nQualTracklets", &nQualTracklets);
-   tr->SetBranchAddress("tlBackPartial", &tlBackPartial);
-
-   tr_tls->SetBranchAddress("event_ID", &tls_event_ID);
-   tr_tls->SetBranchAddress("detIDs", &detIDs);
-   tr_tls->SetBranchAddress("stID", &stID);
-   tr_tls->SetBranchAddress("eleID_exps", &eleID_exps);
-   tr_tls->SetBranchAddress("eleID_closests", &eleID_closests);
-
-   std::map<int, std::vector<std::pair<int, double>>>* hitInfoP;
-   tr->GetObject("hitInfo", hitInfoP);
-   std::map<int, std::vector<std::pair<int, double>>> hitInfo = *hitInfoP;
-
-   //////////
-   bool Trigger_Filter = false; //set to "true" if need to filter hits based on trigger.
-   int det_ID = 32; // pick the detector plane to be analyzed
-   /////////
-
-   int nEntries = tr->GetEntries();
-   for (int i_ent = 0; i_ent < nEntries; i_ent++) {
-
-      tr->GetEntry(i_ent);
-      if(run_ID < xlow || run_ID > xhigh) {continue;}
-      ++nEvents;
-
-      if(Trigger_Filter){
-
-          //if((trigger & 0x1) != 0) { trigger_temp = "NIM1"; ++total_N1;}  //NIM1
-          //if((trigger & 0x2) != 0) { trigger_temp = "NIM2"; ++total_N2;}  //NIM2
-          //if((trigger & 0x4) != 0) { trigger_temp = "NIM3"; ++total_N3;}  //NIM3
-          //if((trigger & 0x8) != 0) { trigger_temp = "NIM4"; total_N4++;}  //NIM4 
-          if((trigger & 0x200) != 0) { trigger_temp = "MATRIX5"; ++total_M5;}  //MATRIX5
-          else{continue;}
-       
-      }
-
-       if (tlBackPartial>0){ 
-            total_bp++;
-            if (num_h3b>0 && num_h3t ==0){++h3b_hits;}
-            if (num_h3t>0 && num_h3b ==0){++h3t_hits;}
-            if (num_h3t >0 && num_h3b >0 ) {++has_hits;} 
-            if (num_h3t ==0 && num_h3b ==0){++no_hits;}
-       }
-      
-      if(dor < 0) {run_num = run_ID; std::cout << "invalid dor"<<std::endl; continue;}
-
-      //since run_num set to the run_ID of the first event in the tree following if condition will be satisfied in the first iteration.
-     
-        for (auto it = hitInfo.begin(); it != hitInfo.end(); it++){
-            if (it->first == det_ID){
-               for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++ ){
-                    htdc->Fill(it2->second);
-                    std::cout<<"Filling :"<<it2->second<<std::endl;
-                }
-            }
-        }
-    } 
-    TCanvas* c1 = new TCanvas(Form("tdc_time of hits in %d from all events", det_ID), Form("tdc_time of hits in %d from all events", det_ID), 1000, 500);
-    
-    htdc->SetLineColor(7);
-    htdc->SetTitle(Form("tdc time - %d", det_ID));
-    htdc->Draw();
-    c1->Update();
-    
-    gSystem->mkdir(Form("hitTDC/%s",trigger_temp.c_str()), 1);
-
-    c1->SaveAs(Form("hitTDC/tdc_h1_%d.png",det_ID));
-    std::cout<<"*************************"<<std::endl;
+int AnaModule::Init(PHCompositeNode* topNode)
+{ 
+  return Fun4AllReturnCodes::EVENT_OK;
 }
 
+int AnaModule::InitRun(PHCompositeNode* topNode)
+{
+  int ret = GetNodes(topNode);
+  if(ret != Fun4AllReturnCodes::EVENT_OK) return ret;
+  
+  eventID = 0;
+  run_ID_temp = 0;
+  tlD0 = 0; tlD1 = 0; tlD2 = 0; tlD3p = 0; tlD3m = 0; tlBackPartial = 0; tlGlobal = 0;
+  MakeTree();
+
+  std::fstream infile("AnaModule/mysql_output.txt");
+
+  while (getline(infile,id))
+  {
+    
+    std::istringstream is( id );
+    is >> r.runID >> r.beginT >> r.endT;
+
+    run_time[r.runID]=std::pair<int,int>(r.beginT, r.endT);
+    }
+	
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int AnaModule::process_event(PHCompositeNode* topNode)
+{
+  nTracklets = trackletVec->size();
+  
+  event_ID = event->get_event_id();
+  run_ID = run -> get_run_id();
+  if (run_ID_temp != run_ID){
+ 	 std::cout << "---> run_ID " << run_ID << std::endl;
+	 std::cout<<  "start time " << run_time.at(run_ID).first <<std::endl;
+	 std::cout<<  "end time " << run_time.at(run_ID).second <<std::endl;
+  	 dor = run_time.at(run_ID).second - run_time.at(run_ID).first;
+ 	 std::cout<<  "run time " << dor <<std::endl;
+
+  }
+  run_ID_temp = run_ID;	
+	if (nTracklets == 0) {return Fun4AllReturnCodes::EVENT_OK;}
+
+	trigger = event->get_trigger();
+
+	for(SQHitVector::Iter it = hitVector->begin(); it != hitVector->end(); ++it)
+ 	{	
+    		int det = (*it)->get_detector_id(); 
+		if ( 30 < det && det < 46 ) {hitInfo[det].push_back(std::pair<int, double> ((*it)->get_element_id(), (*it)->get_tdc_time()));}
+		
+ 	}
+	
+  saveTree->Fill();
+  hitInfo.clear();
+  ++eventID;
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int AnaModule::End(PHCompositeNode* topNode)
+{
+  saveFile->cd();
+  saveTree->Write();
+  tlTree->Write();
+  saveFile->Close();
+
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int AnaModule::GetNodes(PHCompositeNode* topNode)
+{
+  hitVector   = findNode::getClass<SQHitVector>(topNode, "SQHitVector");
+  trackletVec = findNode::getClass<TrackletVector>(topNode, "TrackletVector");
+	event       = findNode::getClass<SQEvent    >(topNode, "SQEvent");
+	run = findNode::getClass<SQRun    >(topNode, "SQRun");
+	
+  if(!hitVector || !trackletVec || !event)
+  {
+    return Fun4AllReturnCodes::ABORTRUN;
+  }
+
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+void AnaModule::MakeTree()
+{
+  saveFile = new TFile(saveName, "RECREATE");
+
+  saveTree = new TTree("save", "Efficiency tree Created by AnaModule");
+	saveTree->Branch("eventID", &eventID, "eventID/I");
+  	saveTree->Branch("event_ID", &event_ID, "event_ID/I");
+	saveTree->Branch("run_ID", &run_ID,"run_ID/I");
+	saveTree->Branch("dor", &dor,"dor/I");
+	saveTree->Branch("trigger", &trigger, "trigger/I");
+	saveTree->Branch("nTracklets", &nTracklets, "nTracklets/I");
+	saveTree->Branch("nQualTracklets", &nQualTracklets, "nQualTracklets/I");
+	saveTree->WriteObject("hitInfo", &hitInfo);
+	
+	saveTree->Branch("tlD0", &tlD0, "tlD0/I");
+	saveTree->Branch("tlD1", &tlD1, "tlD1/I");
+	saveTree->Branch("tlD2", &tlD2, "tlD2/I");
+	saveTree->Branch("tlD3p", &tlD3p, "tlD3p/I");
+	saveTree->Branch("tlD3m", &tlD3m, "tlD3m/I");
+	saveTree->Branch("tlBackPartial", &tlBackPartial, "tlBackPartial/I");
+	saveTree->Branch("tlGlobal", &tlGlobal, "tlGlobal/I");
+}
+
+void AnaModule::registerDetector(TString name)
+{
+  detectorIDs.insert(p_geomSvc->getDetectorID(name.Data()));
+}
